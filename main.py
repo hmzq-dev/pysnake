147003cc61a686d85244ebea7fcbb3f21cd76dc7
hmzq-404 <mhqadir14@gmail.com>
2023-06-06 12:34:53 +0500

    Added apples and score

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
+import random
 import sys
+
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 500
-FPS = 16
-SPEED = 10
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+FPS = 12
+ENTITY_SIZE = 20
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption("PySnake")
@@ -14,11 +16,12 @@ pygame.display.set_caption("PySnake")
 
 snake = []
 direction = "right"
+score = 0
 
 class SnakeBody(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.surf = pygame.Surface((25, 25))
+        self.surf = pygame.Surface((ENTITY_SIZE, ENTITY_SIZE))
         self.surf.fill("green")
 
         if len(snake) == 0:
@@ -31,11 +34,32 @@ class SnakeBody(pygame.sprite.Sprite):
                 top=snake[-1].rect.y
             )
 
-def grow_snake(n):
+
+class Apple(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((ENTITY_SIZE, ENTITY_SIZE))
+        self.surf.fill("red")
+
+        if score == 0:
+            self.rect = self.surf.get_rect(
+                center=(SCREEN_WIDTH*(3/4), SCREEN_HEIGHT/2)
+            )
+        else:
+            randx = random.randint(0, SCREEN_WIDTH-ENTITY_SIZE)
+            randy = random.randint(0, SCREEN_HEIGHT-ENTITY_SIZE)
+            self.rect = self.surf.get_rect(
+                x=randx,
+                y=randy
+            )
+
+
+def grow_snake(n=1):
     for i in range(0, n):
         snake.append(SnakeBody())
 
 grow_snake(6)
+apple = Apple()
 
 
 while True:
@@ -60,24 +84,35 @@ while True:
             tmp = (snake_body.rect.x, snake_body.rect.y)
             new_pos = (snake_body.rect.x, snake_body.rect.y)
             if direction == "up":
-                snake_body.rect.move_ip(0, -SPEED)
+                snake_body.rect.move_ip(0, -ENTITY_SIZE)
             elif direction == "down":
-                snake_body.rect.move_ip(0, SPEED)
+                snake_body.rect.move_ip(0, ENTITY_SIZE)
             elif direction == "right":
-                snake_body.rect.move_ip(SPEED, 0)
+                snake_body.rect.move_ip(ENTITY_SIZE, 0)
             elif direction == "left":
-                snake_body.rect.move_ip(-SPEED, 0)
+                snake_body.rect.move_ip(-ENTITY_SIZE, 0)
         else:
             tmp = (snake_body.rect.x, snake_body.rect.y)
             snake_body.rect.x, snake_body.rect.y = new_pos
             new_pos = tmp
 
-    screen.fill("black")
+    # Check if apple is eaten
+    if pygame.sprite.collide_rect(snake[0], apple):
+        score += 1
+        grow_snake()
+        print(score)
+        apple = Apple()
+        # Checking if new apple collides with the snake
+        for snake_body in snake:
+            if pygame.sprite.collide_rect(snake_body, apple):
+                apple = Apple()
 
+    screen.fill("black")
     for snake_body in snake:
         screen.blit(snake_body.surf, snake_body.rect)
 
-    pygame.display.flip()
+    screen.blit(apple.surf, apple.rect)
 
+    pygame.display.flip()
 
     clock.tick(FPS)
